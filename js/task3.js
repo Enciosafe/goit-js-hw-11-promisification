747aaaf5871b583bfcddbe588b3a46806e2f134b
Stanislav Matvieiev <enciosafe@gmail.com>
2021-03-25 20:01:21 +0200

    3

--- js/task3.js
+++ js/task3.js
@@ -4,22 +4,27 @@ const randomIntegerFromInterval = (min, max) => {
 
 const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500);
+  
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
     const canProcess = Math.random() > 0.3;
-
+    
     if (canProcess) {
-      resolve(transaction.id, delay);
+      resolve({
+        id: transaction.id,
+        time: delay,
+      })
     } else {
       reject(transaction.id);
     }
   }, delay);
   })
+ 
 }
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({id, time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
 };
 
 const logError = id => {
